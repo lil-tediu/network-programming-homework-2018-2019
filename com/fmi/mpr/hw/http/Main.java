7fb6cf2903eeb8f6b930b249582abe937f81f7a4
teodor <teodor.yolovski@gmail.com>
2019-01-02 20:37:19 +0200

    Added post request processing

--- com/fmi/mpr/hw/http/Main.java
+++ com/fmi/mpr/hw/http/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -64,29 +65,25 @@ public class Main {
 		try (BufferedInputStream br = new BufferedInputStream(client.getInputStream());
 				PrintStream ps = new PrintStream(client.getOutputStream(), true)) {
 
-			String response = read(ps, br);
+			String response = read(ps, br, client);
 			write(ps, response);
 		}
 
 	}
 
 	private void write(PrintStream ps, String response) {
-		if (ps != null) 
-		{
+		if (ps != null) {
+
+			ps.println("HTTP/1.1 200 OK");
 			ps.println();
-			ps.println( "<!DOCTYPE html>\n" + 
-						"<form action=\"upload.php\" method=\"POST\" enctype=\"multipart/form-data\">\n" + 
-						"Select file to upload:\n" +
-						"<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">\n" +
-						"<input type=\"submit\" value=\"Upload File\" name=\"submit\">\n" +
-						"</form>" +
-					"<h2>" + (response == null || response.trim().isEmpty() ? "" : response) + "</h2>" +
-					"</body>\n" + 
-					"</html>");
+			ps.println("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + "	<title></title>\n" + "</head>\n"
+					+ "<body>\n" + "<form action=\"/action_page.php\">\n"
+					+ "			  <input type=\"file\" name=\"pic\" accept=\"image/*\">\n"
+					+ "			  <input type=\"submit\">\n" + "			</form> " + "</body>\n" + "</html>");
 		}
 	}
 
-	private String read(PrintStream ps, BufferedInputStream bis) throws IOException {
+	private String read(PrintStream ps, BufferedInputStream bis, Socket client) throws IOException {
 
 		if (bis != null) {
 			StringBuilder request = new StringBuilder();
@@ -102,16 +99,17 @@ public class Main {
 				}
 			}
 
-			return parseRequest(ps, request.toString());
+			return parseRequest(ps, request.toString(), client);
 		}
 		return "Error";
 	}
 
-	private String parseRequest(PrintStream ps, String request) throws IOException {
+	private String parseRequest(PrintStream ps, String request, Socket client) throws IOException {
 
 		System.out.println(request);
 
-		String firstHeader = request.split("\n")[0];
+		String[] lines = request.split("\n");
+		String firstHeader = lines[0];
 		String type = firstHeader.split(" ")[0];
 		System.out.println(firstHeader);
 		String uri = firstHeader.split(" ")[1];
@@ -122,14 +120,41 @@ public class Main {
 		if (type.equals("GET")) {
 			return get(ps, typeOfExtension);
 		} else if (type.equals("POST")) {
-			return post(ps, typeOfExtension);
+			return post(ps, client, lines);
 		}
 
 		return null;
 	}
 
-	private String post(PrintStream ps, String typeOfExtension) {
-		// TODO Auto-generated method stub
+	private String post(PrintStream ps, Socket client, String[] lines) throws IOException {
+		String header = lines[0];
+		String url = header.split(" ")[1];
+
+		if (url.length() != 1) {
+
+			url = url.substring(1);
+		}
+
+		if (url.equals("upload.php")) {
+			StringBuilder body = new StringBuilder();
+
+			boolean readBody = false;
+			for (String line : lines) {
+				if (readBody) {
+
+					body.append(line);
+				}
+
+				if (line.trim().isEmpty()) {
+
+					readBody = true;
+				}
+
+			}
+
+			return parseBody(client, body.toString());
+		}
+
 		return null;
 	}
 
@@ -148,9 +173,7 @@ public class Main {
 
 				ps.println();
 				ps.println("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + "	<title></title>\n" + "</head>\n"
-						+ "<body>\n" + "<form action=\"/action_page.php\">\n"
-						+ "			  <input type=\"file\" name=\"pic\" accept=\"image/*\">\n"
-						+ "			  <input type=\"submit\">\n" + "			</form> " + "</body>\n" + "</html>");
+						+ "<body>\n" + "			  Error! This file doesn't exist \n" + "</body>\n" + "</html>");
 			}
 		}
 
@@ -163,11 +186,7 @@ public class Main {
 
 				ps.println();
 				ps.println("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + "	<title></title>\n" + "</head>\n"
-						+ "<body>\n" + "<form action=\"/action_page.php\">\n"
-						+ "			  <input type=\"file\" name=\"pic\" accept=\"image/*\">\n"
-						+ "			  <input type=\"submit\">\n" + "			</form> " + "</body>\n" + "</html>");
-				
-				ps.println("Da ti eba maikata kuha!!!");
+						+ "<body>\n" + "			  Error! This file doesn't exist \n" + "</body>\n" + "</html>");
 			}
 		}
 
@@ -181,9 +200,7 @@ public class Main {
 
 				ps.println();
 				ps.println("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n" + "	<title></title>\n" + "</head>\n"
-						+ "<body>\n" + "<form action=\"/action_page.php\">\n"
-						+ "			  <input type=\"file\" name=\"pic\" accept=\"image/*\">\n"
-						+ "			  <input type=\"submit\">\n" + "			</form> " + "</body>\n" + "</html>");
+						+ "<body>\n" + "			  Error! This file doesn't exist \n" + "</body>\n" + "</html>");
 			}
 		}
 		return null;
@@ -195,18 +212,17 @@ public class Main {
 
 		FileInputStream fis = new FileInputStream(path);
 
-
 		int bytesRead = 0;
 		byte[] buffer = new byte[8192];
- 		
- 		while ((bytesRead = fis.read(buffer, 0, 8192)) > 0) {
- 			ps.write(buffer, 0, bytesRead);
- 		}
- 		
- 		ps.flush();
- 		System.out.println("Send txt");
+
+		while ((bytesRead = fis.read(buffer, 0, 8192)) > 0) {
+			ps.write(buffer, 0, bytesRead);
+		}
+
+		ps.flush();
+		System.out.println("Send txt");
 		fis.close();
-		
+
 	}
 
 	private void sendPic(PrintStream ps) throws IOException {
@@ -215,18 +231,17 @@ public class Main {
 
 		FileInputStream fis = new FileInputStream(path);
 
-
 		int bytesRead = 0;
 		byte[] buffer = new byte[4096];
- 		
- 		while ((bytesRead = fis.read(buffer, 0, 4096)) > 0) {
- 			ps.write(buffer, 0, bytesRead);
- 		}
- 		
- 		ps.flush();
- 		System.out.println("Send pic");
- 		fis.close();
-		
+
+		while ((bytesRead = fis.read(buffer, 0, 4096)) > 0) {
+			ps.write(buffer, 0, bytesRead);
+		}
+
+		ps.flush();
+		System.out.println("Send pic");
+		fis.close();
+
 	}
 
 	private void sendVideo(PrintStream ps) throws IOException {
@@ -237,20 +252,71 @@ public class Main {
 
 		int bytesRead = 0;
 		byte[] buffer = new byte[8192];
- 		
- 		while ((bytesRead = fis.read(buffer, 0, 8192)) > 0) {
- 			ps.write(buffer, 0, bytesRead);
- 		}
- 		
- 		ps.flush();
- 		System.out.println("Send video");
- 		fis.close();
+
+		while ((bytesRead = fis.read(buffer, 0, 8192)) > 0) {
+			ps.write(buffer, 0, bytesRead);
+		}
+
+		ps.flush();
+		System.out.println("Send video");
+		fis.close();
 	}
 
-	private String parseBody(String body) {
+	private String parseBody(Socket client, String body) throws IOException {
+		if (body != null && !body.trim().isEmpty()) {
 
+			String[] operands = body.split(";");
+			this.file = operands[2].split("=")[1].split("\"")[1];
+
+			String type = this.file.split("\\.")[1];
+			BufferedInputStream bis = new BufferedInputStream(client.getInputStream());
+			String data = null;
+			PrintStream ps = new PrintStream(client.getOutputStream(), true);
+
+			if (type.equals("jpg") || type.equals("jpeg") || type.equals("bmp") || type.equals("mp4")
+					|| type.equals("avi")) {
+
+				data = sendMedia(bis, ps);
+			}
+
+			if (type.equals("txt")) {
+
+				data = sendTextFiles(bis, ps);
+			}
+
+			File file = new File(this.file);
+			FileOutputStream is = new FileOutputStream(file.getAbsolutePath());
+			is.write(data.getBytes());
+			is.close();
+			System.out.println("File sent!");
+		}
 
 		return null;
+	}
+
+	private String sendTextFiles(BufferedInputStream bis, PrintStream ps) throws IOException {
+		int bytesRead = 0;
+		byte[] buffer = new byte[8192];
+
+		while ((bytesRead = bis.read(buffer, 0, 8192)) > 0) {
+
+			ps.write(buffer, 0, bytesRead);
+		}
+
+		return ps.toString();
+
+	}
+
+	private String sendMedia(BufferedInputStream bis, PrintStream ps) throws IOException {
+		int bytesRead = 0;
+		byte[] buffer = new byte[8192];
+
+		while ((bytesRead = bis.read(buffer, 0, 8192)) > 0) {
+
+			ps.write(buffer, 0, bytesRead);
+		}
+
+		return ps.toString();
 	}
 
 	public static void main(String[] args) throws IOException {
